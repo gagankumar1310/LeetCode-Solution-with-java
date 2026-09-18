c11a84020937f993c174d60af9f82fef0170dd50
Gagan <gagansingh13october2000@gmail.com>
2026-09-19 00:49:04 +0530

    Time: 0 ms (100%), Space: 44 MB (16.44%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,23 +1,21 @@
 class Solution {
     public void rotate(int[][] arr) {
-        int n = arr.length ;  
-        int m = arr[0].length ;
-        for(int i = 0 ; i<n ; i++){
+        int n = arr.length ; n = arr[0].length ; 
+        for(int i = 0 ; i<n ;i++){
             for(int j = 0 ; j<i ; j++){
                 int temp = arr[i][j] ;
                 arr[i][j] = arr[j][i] ;
-                arr[j][i] = temp ;           
+                arr[j][i] = temp ; 
             }
         }
-        // reverse row 
-        for(int i = 0 ; i<n ; i++){
-            int s = 0 ; int e = m-1 ;
+        for(int i = 0 ;i<n ; i++){
+            int s = 0 ; int e = n-1 ; 
             while(s<e){
                 int temp = arr[i][s] ;
-                arr[i][s] = arr[i][e] ;
-                arr[i][e] = temp ;
-                s++ ;
-                e-- ;
+                arr[i][s]= arr[i][e] ;
+                arr[i][e] = temp ; 
+                s++;
+                e-- ; 
             }
         }
     }
